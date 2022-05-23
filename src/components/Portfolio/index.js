eecd4fb0bacda6f99c67cbafcb0f8c7c285ca72a
Michael Novajosky <mikenovajosky@yahoo.com>
2022-05-23 13:34:45 -0700

    portfolio refactor

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,23 +7,22 @@ import './index.scss'
 
 export default function Portfolio() {
   return (
-    <div className= "container portfolio">
-        <div className='row g-3'>
-            <div className="col-14 col-md-6 col-lg-4">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">MatchPlay</h5>
-                        <p className="card-text">A MERN-stack web application that allows users to 
-                            share and connect with other like-minded golfers.
-                        </p>
-                    </div>
-                    <div className="card-footer">
-                        <a className="deployed-link" target="_blank" href="https://matchplaygolf.herokuapp.com/"><FontAwesomeIcon icon={faPaperclip} color="#4d4d4e" /></a>
-                        <a className="github-link" target="_blank" href="https://github.com/novajosky/MatchPlay"><FontAwesomeIcon icon={faGithub} color="#4d4d4e" /></a>
-                    </div>
+    <>
+        <div className= "container portfolio-page">
+            <div className= "text-zone">
+                <div className="card-body">
+                    <h5 className="card-title">MatchPlay</h5>
+                    <p className="card-text">A MERN-stack web application that allows users to 
+                        share and connect with other like-minded golfers.
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <a className="deployed-link" target="_blank" href="https://matchplaygolf.herokuapp.com/"><FontAwesomeIcon icon={faPaperclip} color="#4d4d4e" /></a>
+                    <br />
+                    <a className="github-link" target="_blank" href="https://github.com/novajosky/MatchPlay"><FontAwesomeIcon icon={faGithub} color="#4d4d4e" /></a>
                 </div>
             </div>
         </div>
-    </div>
+    </>
   )
 }
